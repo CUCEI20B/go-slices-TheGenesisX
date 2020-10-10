a2ba14c427b39cef4d6b7896f096bc0690c0e890
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-10-10 14:24:42 -0500

    Programa terminado

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n, i uint64 = 0, 0
+	var value, suma int64 = 0, 0
+	var s []int64
+
+	fmt.Scan(&n)
+
+	for i = 0; i < n; i++ {
+		fmt.Scan(&value)
+		s = append(s, value)
+		suma += value
+	}
+
+	fmt.Println(suma)
 }
